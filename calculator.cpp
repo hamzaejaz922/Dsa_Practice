b7942c2311cff868ed9bbefbdac3dff10ae1c57b
Muhammad Hamza <hamzaejaz3136@gmail.com>
2026-08-19 21:06:52 +0500

    calculator app using C++

---
+++ calculator.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	
+int sum;
+int subt;
+int mult;
+float div;
+int mod;
+
+int a;
+int b;
+int c;
+
+                     cout<<"--------MENU DREIVEN FOR ARTHEMATIC CALCULATION-------"<<endl;
+                     
+cout<<"Enter the first number:"<<endl;
+cout<<"Enter the second number:"<<endl;
+cout<<"Enter the third number:"<<endl;
+
+sum=a+b+c;
+subt=a-b-c;
+mult=a*b*c;
+div=a/b/c;
+mod=a%b%c;
+
+cout<<"The sum of three existing number is:"<<sum<<endl;
+cout<<"The substarction of three existing number is:"<<subt<<endl;
+cout<<"The multiplication of three existing number is:"<<mult<<endl;
+cout<<"The division of three existing number is:"<<div<<endl;
+cout<<"The mode of three existing number is:"<<mod<<endl;
+
+return 0;
+
+}
+
+
